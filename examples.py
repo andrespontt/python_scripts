0e55b157db58b5c6c750b03278c9e9c00bd4e3e8
Andres Pontt <andrespontt@gmail.com>
2024-03-09 12:51:58 -0500

    update the file

--- examples.py
+++ examples.py
@@ -5,3 +5,13 @@ path = '.'
 files = os.listdir(path)
 print(files)
 
+
+# list recursively all files in the current directory
+for root, dirs, files in os.walk(path):
+    for file in files:
+        print(file)
+
+# list all files in the current directory with a specific extension
+extension = '.txt'
+files = [file for file in os.listdir(path) if file.endswith(extension)]
+print(files)
